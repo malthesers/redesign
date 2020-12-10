0223fe3b290b52a279811dbfdc7ad526a071704a
malt9830 <70881301+malt9830@users.noreply.github.com>
2020-12-10 07:58:18 +0100

    tilføjet html og css sider

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onscroll = function () {
-    console.log("scroll")
-    if (window.scrollY > 25) {
-        document.querySelector(".header").classList.add("active");
+    console.log("scroll");
+    if (window.scrollY > 25 && window.outerWidth > 650) {
+        document.querySelector("#header").classList.add("active");
     } else {
-        document.querySelector(".header").classList.remove("active");
+        document.querySelector("#header").classList.remove("active");
     }
 }
