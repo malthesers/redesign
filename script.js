3930bc356b4a2cf95958db937892b1fe1ffa75e6
malt9830 <70881301+malt9830@users.noreply.github.com>
2020-12-11 10:23:49 +0100

    tilføjet header

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+    document.querySelector("#header").classList.remove("active");
+}
+
 window.onscroll = function () {
     console.log("scroll");
-    if (window.scrollY > 25 && window.outerWidth > 650) {
+    if (window.scrollY > 25 /*&& window.outerWidth > 650*/ ) {
         document.querySelector("#header").classList.add("active");
     } else {
         document.querySelector("#header").classList.remove("active");
